547d45159bdcade64d205670e4762f8d368366ce
Meen Khatri <meenkhatri@MeenKhatrisMBP.fios-router.home>
2019-06-19 01:57:19 -0400

    Test my code

---
+++ MY_JAVA_CODE/src/AddFunction.java
@@ -0,0 +1,19 @@
+
+public class AddFunction {
+
+	
+		public static void main(String arng[]){
+			
+			int a =25, b =35;
+
+			sum(a,b);
+
+		}
+		// This is sum method
+		 static void sum(int a, int b){
+		int sum=0;
+		sum=a+b;
+		System.out.println("Sum = "+ sum);
+
+		}
+		}	
